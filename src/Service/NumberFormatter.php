71becc914bc22b2afee66986431656c08de43469
Dangyras Stepanavicius <omnus@windowslive.com>
2018-05-29 22:38:10 +0300

    Push

--- src/Service/NumberFormatter.php
+++ src/Service/NumberFormatter.php
@@ -27,17 +27,19 @@ class NumberFormatter
         if ($abs >= 999500) {
             return $symbol . number_format(round($abs / 1000000, 2), 2) . 'M';
         }
+
         if ($abs >= 99950 && $abs < 999500) {
             return $symbol . number_format(round($abs / 1000, 0), 0) . 'K';
         }
+
         if ($abs >= 1000 && $abs < 99950) {
             return $symbol . number_format(round($abs, 0), 0, '', ' ');
         }
+        
         if (round($abs, 0) === round($abs, 2)) {
             return $symbol . number_format(round($abs, 0), 0, '', ' ');
         }
 
         return $symbol . number_format(round($abs, 2), 2);
     }
-
 }
